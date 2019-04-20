4e14ff4cb7ba82a081bdd5bb713353574b4d13db
John Pignata <john@pignata.com>
2019-04-20 16:03:02 -0400

    Add favicon
    * Fix indentation

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const WebSocket = require('ws');
 const ws = new WebSocket.Server({ port: process.env.PORT });
 
 ws.on('connection', (conn) => {
-	conn.on('message', (data) => {
-		ws.clients.forEach((client) => {
-			if (client !== conn && client.readyState === WebSocket.OPEN) {
-				client.send(data);
-			}
-		});
-	});
+  conn.on('message', (data) => {
+    ws.clients.forEach((client) => {
+      if (client !== conn && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
 });
